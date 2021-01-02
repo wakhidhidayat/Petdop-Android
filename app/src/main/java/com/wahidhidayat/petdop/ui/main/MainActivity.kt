93ff3e864d962bdf055f4f6b08eec641e66ba530
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-02 22:21:47 +0700

    added bottom navigation

--- app/src/main/java/com/wahidhidayat/petdop/ui/main/MainActivity.kt
+++ app/src/main/java/com/wahidhidayat/petdop/ui/main/MainActivity.kt
@@ -2,11 +2,58 @@ package com.wahidhidayat.petdop.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wahidhidayat.petdop.R
+import com.wahidhidayat.petdop.ui.account.AccountFragment
+import com.wahidhidayat.petdop.ui.bookmark.BookmarkFragment
+import com.wahidhidayat.petdop.ui.explore.ExploreFragment
+import com.wahidhidayat.petdop.ui.home.HomeFragment
+import com.wahidhidayat.petdop.ui.upload.UploadFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(HomeFragment())
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+
+        when (item.itemId) {
+            R.id.menu_home -> {
+                fragment = HomeFragment()
+            }
+            R.id.menu_account -> {
+                fragment = AccountFragment()
+            }
+            R.id.menu_bookmark -> {
+                fragment = BookmarkFragment()
+            }
+            R.id.menu_explore -> {
+                fragment = ExploreFragment()
+            }
+            R.id.menu_upload -> {
+                fragment = UploadFragment()
+            }
+        }
+
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }
