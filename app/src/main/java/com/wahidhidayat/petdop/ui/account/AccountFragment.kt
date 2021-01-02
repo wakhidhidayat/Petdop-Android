d1c21f4422cf383e6301a347a60f5c4e059e0642
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-02 23:25:32 +0700

    show user data in AccountFragment

--- app/src/main/java/com/wahidhidayat/petdop/ui/account/AccountFragment.kt
+++ app/src/main/java/com/wahidhidayat/petdop/ui/account/AccountFragment.kt
@@ -1,34 +1,22 @@
 package com.wahidhidayat.petdop.ui.account
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.wahidhidayat.petdop.R
+import com.wahidhidayat.petdop.ui.login.LoginActivity.Companion.TAG
+import kotlinx.android.synthetic.main.fragment_account.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private val mUser = FirebaseAuth.getInstance().currentUser
+private val mDb = FirebaseFirestore.getInstance()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +26,37 @@ class AccountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userRef = mDb.document("users/${mUser?.email.toString()}")
+        userRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                getUser(
+                    documentSnapshot.getString("name"),
+                    documentSnapshot.getString("email"),
+                    documentSnapshot.getString("phone"),
+                    documentSnapshot.getString("address"),
+                    documentSnapshot.getString("avatar")
+                )
+                pb_account.visibility = View.GONE
+                Log.d("AccountFragment", "DocumentSnapshot data: ${documentSnapshot.getString("name")}.")
             }
+            .addOnFailureListener { exception ->
+                Log.d("AccountFragment", "get failed with ", exception)
+            }
+    }
+
+    private fun getUser(name: String?, email: String?, phone: String?, address: String?, avatar: String?) {
+        et_name.setText(name)
+        et_address.setText(address)
+        et_phone.setText(phone)
+        et_email.setText(email)
+
+        if (avatar != null) {
+            Glide.with(this)
+                .load(avatar)
+                .into(image_avatar)
+        }
     }
 }
