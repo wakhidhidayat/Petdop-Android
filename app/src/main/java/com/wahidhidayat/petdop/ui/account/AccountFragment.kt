28a6154ed852496d1eb70540c17486ee1cbc162f
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-03 23:16:11 +0700

    added take picture from camera

--- app/src/main/java/com/wahidhidayat/petdop/ui/account/AccountFragment.kt
+++ app/src/main/java/com/wahidhidayat/petdop/ui/account/AccountFragment.kt
@@ -1,22 +1,49 @@
 package com.wahidhidayat.petdop.ui.account
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Base64
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.wahidhidayat.petdop.R
 import com.wahidhidayat.petdop.ui.login.LoginActivity.Companion.TAG
 import kotlinx.android.synthetic.main.fragment_account.*
-
-private val mUser = FirebaseAuth.getInstance().currentUser
-private val mDb = FirebaseFirestore.getInstance()
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AccountFragment : Fragment() {
+    companion object {
+        const val CAMERA_PERMISSION_CODE = 101
+        const val CAMERA_REQUEST_CODE = 102
+    }
+
+    private val mUser = FirebaseAuth.getInstance().currentUser
+    private val mDb = FirebaseFirestore.getInstance()
+    private val userRef = mDb.document("users/${mUser?.email.toString()}")
+    private val mStorageRef = FirebaseStorage.getInstance().reference
+    private var imageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +56,6 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val userRef = mDb.document("users/${mUser?.email.toString()}")
         userRef.get()
             .addOnSuccessListener { documentSnapshot ->
                 getUser(
@@ -45,6 +71,79 @@ class AccountFragment : Fragment() {
             .addOnFailureListener { exception ->
                 Log.d("AccountFragment", "get failed with ", exception)
             }
+
+        btn_camera.setOnClickListener {
+            askCameraPermission()
+        }
+
+        btn_update_user.setOnClickListener {
+            pb_account.visibility = View.VISIBLE
+
+            userRef.update(mapOf(
+                "name" to et_name.text.toString(),
+                "address" to et_address.text.toString(),
+                "phone" to et_phone.text.toString()
+            )).addOnSuccessListener {
+                pb_account.visibility = View.GONE
+                Toast.makeText(activity, "Profil berhasil di update!", Toast.LENGTH_SHORT).show();
+            }
+                .addOnFailureListener{e-> Log.w(TAG, "Error updating document",e)}
+
+            fileUpload()
+        }
+    }
+
+    private fun askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this.context!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.activity!!, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        } else {
+            capturePhoto()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == CAMERA_PERMISSION_CODE) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                capturePhoto()
+            } else {
+                Toast.makeText(activity, "Camera Permission is required!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CAMERA_REQUEST_CODE) {
+            imageUri = data?.data
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image_avatar.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun fileChooser() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 1)
+    }
+
+    private fun fileUpload() {
+        val imageRef = mStorageRef.child("avatars/${UUID.randomUUID()}")
+        imageRef.putFile(imageUri!!)
+                .addOnSuccessListener {
+                    Toast.makeText(activity, "Upload success!", Toast.LENGTH_SHORT).show()
+                    imageRef.downloadUrl.addOnSuccessListener {
+                        Log.d("uri", it.toString())
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e("AccountFragment", it.toString())
+                }
     }
 
     private fun getUser(name: String?, email: String?, phone: String?, address: String?, avatar: String?) {
