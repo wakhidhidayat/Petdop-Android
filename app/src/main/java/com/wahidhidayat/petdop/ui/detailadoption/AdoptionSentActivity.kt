d356970a222beb87eb7b3b0a146f0e839fc4cb96
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-10 11:01:32 +0700

    added layout detail adoption

---
+++ app/src/main/java/com/wahidhidayat/petdop/ui/detailadoption/AdoptionSentActivity.kt
@@ -0,0 +1,12 @@
+package com.wahidhidayat.petdop.ui.detailadoption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wahidhidayat.petdop.R
+
+class AdoptionSentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adoption_sent)
+    }
+}
